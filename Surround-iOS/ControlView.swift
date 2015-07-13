77d6453bbf5593d6c902dcce02c75df077831703
ctong1234 <calvinytong@gmail.com>
2015-07-13 14:33:49 -0700

    refactored into UIView

--- Surround-iOS/ControlView.swift
+++ Surround-iOS/ControlView.swift
@@ -1,67 +1,168 @@
-////
-////  ControlView.swift
-////  Surround-iOS
-////
-////  Created by Calvin on 7/10/15.
-////  Copyright (c) 2015 Calvin. All rights reserved.
-////
+//
+//  ControlView.swift
+//  Surround-iOS
+//
+//  Created by Calvin on 7/10/15.
+//  Copyright (c) 2015 Calvin. All rights reserved.
+//
 
 //  TODO- MAKE COLORED BUTTONS INTO A CUSTOM VIEW SO WE CAN MAKE IT LOOK COOLER
-//
-//import Foundation
-//import UIKit
-//
-//class ControlView : UIView
-//{
-//    var redbutton : UIButton
-//    var bluebutton : UIButton
-//    var greenbutton : UIButton
-//    
-//    required init(coder aDecoder: NSCoder)
-//    {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override init(frame: CGRect)
-//    {
-//        super.init(frame: frame)
-//        
-//
-//    }
-//    
-//    
-//    func drawButton()
-//    {
-//        
-//    }
-//    
-//    private func drawButtons()
-//    {
-//        let middlebuttonx : CGFloat = self.bounds.width / 2 - circlebuttonsize / 2
-//        let circlebuttony : CGFloat = (self.bounds.height / 4) * 3 + circlebuttonsize / 4
-//        
-//        makeColoredButton(redbutton, action: "redButtonPressed", color: UIColor.redColor(), xcoor:  middlebuttonx - 2 * circlebuttonsize, ycoor: circlebuttony)
-//        makeColoredButton(bluebutton, action: "blueButtonPressed", color: UIColor.blueColor(), xcoor: middlebuttonx, ycoor: circlebuttony)
-//        makeColoredButton(greenbutton, action: "greenButtonPressed", color: UIColor.greenColor(), xcoor: middlebuttonx + 2 * circlebuttonsize, ycoor: circlebuttony)
-//        
-//    }
-//    
-//    //draws a colored circular button at pos xcoor, ycoor
-//    private func makeColoredButton(button : UIButton, action : Selector, color : UIColor, xcoor : CGFloat, ycoor : CGFloat)
-//    {
-//        
-//        button.frame = CGRectMake(xcoor, ycoor, circlebuttonsize, circlebuttonsize)
-//        button.layer.cornerRadius = 0.5 * button.bounds.size.width
-//        button.backgroundColor = color
-//        button.addTarget(self, action: action, forControlEvents: UIControlEvents.TouchUpInside)
-//        self.addSubview(button)
-//        
-//        //only blurs in the square frame can't seem to get it to blur a circle
-//        //        let blur = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
-//        //        blur.frame = button.frame
-//        //        blur.layer.cornerRadius = 0.5 * button.bounds.size.width
-//        //        self.view.addSubview(blur)
-//        
-//    }
-//    
-//}
+
+import Foundation
+import UIKit
+import Socket_IO_Client_Swift
+class ControlView : UIView
+{
+    var redbutton : UIButton!
+    var bluebutton : UIButton!
+    var greenbutton : UIButton!
+    
+    var circlebuttonsize : CGFloat! //= (self.view.bounds.height / 4 - 10) * (2/3) as CGFloat can't assign up here even though I would like to
+
+    var sc = SocketClient()
+
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        sizeView()
+        self.redbutton = UIButton()
+        self.bluebutton = UIButton()
+        self.greenbutton = UIButton()
+        drawButtons()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "orientationChanged", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "watchNotification:", name: "watchNotification", object: nil)
+    }
+    
+    override func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool {
+        for subview in subviews as! [UIView] {
+            if !subview.hidden && subview.alpha > 0 && subview.userInteractionEnabled && subview.pointInside(convertPoint(point, toView: subview), withEvent: event) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func redrawView()
+    {
+        sizeView()
+        drawButtons()
+    }
+    
+    
+    private func drawButtons()
+    {
+        let middlebuttonx : CGFloat = UIScreen.mainScreen().bounds.size.width / 2 - circlebuttonsize / 2
+        let circlebuttony : CGFloat = (UIScreen.mainScreen().bounds.size.height / 4) * 3 + circlebuttonsize / 4
+        
+        makeColoredButton(redbutton, action: "redButtonPressed", color: UIColor.redColor(), xcoor:  middlebuttonx - 2 * circlebuttonsize, ycoor: circlebuttony)
+        makeColoredButton(bluebutton, action: "blueButtonPressed", color: UIColor.blueColor(), xcoor: middlebuttonx, ycoor: circlebuttony)
+        makeColoredButton(greenbutton, action: "greenButtonPressed", color: UIColor.greenColor(), xcoor: middlebuttonx + 2 * circlebuttonsize, ycoor: circlebuttony)
+        
+    }
+    
+    //draws a colored circular button at pos xcoor, ycoor
+    private func makeColoredButton(button : UIButton, action : Selector, color : UIColor, xcoor : CGFloat, ycoor : CGFloat)
+    {
+        
+        button.frame = CGRectMake(xcoor, ycoor, circlebuttonsize, circlebuttonsize)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.backgroundColor = color
+        button.addTarget(self, action: action, forControlEvents: UIControlEvents.TouchUpInside)
+        self.addSubview(button)
+        
+        //only blurs in the square frame can't seem to get it to blur a circle
+        //        let blur = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
+        //        blur.frame = button.frame
+        //        blur.layer.cornerRadius = 0.5 * button.bounds.size.width
+        //        self.view.addSubview(blur)
+        
+    }
+    
+    //function called when the red button is pressed
+    func redButtonPressed()
+    {
+        //add code for red button here
+        println("red pressed")
+        sc.socket.emit("change color", 1.0, 0.0, 0.0)
+    }
+    
+    //function called when the blue button is pressed
+    func blueButtonPressed()
+    {
+        //add code for blue button here
+        println("blue pressed")
+        sc.socket.emit("change color", 0.0, 0.0, 1.0)
+    }
+    
+    //function called when the green button is pressed
+    func greenButtonPressed()
+    {
+        //add code for green button here
+        println("green pressed")
+        sc.socket.emit("change color", 0.0, 1.0, 0.0)
+    }
+    
+    
+    //function for handling the notification from the watch. unpacks the sent message and then runs a switch to determine which method to run
+    func watchNotification(notification : NSNotification)
+    {
+        //println("hi from instide the notification")
+        var color = notification.object as! String
+        switch(color)
+        {
+        case "red":
+            redButtonPressed()
+        case "blue":
+            blueButtonPressed()
+        case "green":
+            greenButtonPressed()
+        default:
+            return
+            //shouldn't happen but who knows
+        }
+    }
+    
+    private func sizeView()
+    {
+        println("sizing view")
+        var orientation = UIDevice.currentDevice().orientation
+        var bounds = UIScreen.mainScreen().bounds
+        var width = bounds.size.width
+        var height = bounds.size.height
+        if orientation == UIDeviceOrientation.LandscapeLeft || orientation == UIDeviceOrientation.LandscapeRight
+        {
+            circlebuttonsize = (height / 4) * (2/3) as CGFloat
+        }
+        else
+        {
+            circlebuttonsize = (width / 4) * (2/3) as CGFloat
+        }
+    }
+    
+    //function called when the device changes orientation
+    func orientationChanged()
+    {
+        if(UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation))
+        {
+            redrawView()
+        }
+        
+        if(UIDeviceOrientationIsPortrait(UIDevice.currentDevice().orientation))
+        {
+            redrawView()
+        }
+        
+    }
+    
+    
+    
+
+    
+}
--- Surround-iOS/RGBViewController.swift
+++ Surround-iOS/RGBViewController.swift
@@ -16,9 +16,13 @@ class RGBViewController : UIViewController
     private var streamviewframe : CGRect!
     var button1 : UIButton = UIButton()
     
-    var redbutton = UIButton()
-    var bluebutton = UIButton()
-    var greenbutton = UIButton()
+    var redbutton : UIButton = UIButton()
+    var bluebutton : UIButton = UIButton()
+    var greenbutton : UIButton = UIButton()
+    
+    var cv : ControlView!
+    
+    var buttonsDisplayed : Bool = true
     
     var sc = SocketClient()
     
@@ -29,24 +33,27 @@ class RGBViewController : UIViewController
     //viewDidLoad method to do initial screen setup
     override func viewDidLoad()
     {
-//        sc.connect({(success: Bool!, error: NSError!) -> Void in
-//        if success == true
-//        {
-//            self.testURL = NSURL(string: self.sc.url)
-            self.testURL = NSURL(string: "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov")
-            self.sizeView()
-            self.startStream()
-            self.drawButtons()
-            self.button1.tag = 1
-            
-            //listens to the device orientation, runs orientationChanged when the device is flipped
-            NSNotificationCenter.defaultCenter().addObserver(self, selector: "orientationChanged", name: UIDeviceOrientationDidChangeNotification, object: nil)
-            
-            //listens to watchNotifications, runs watchNotification when the device is flipped
-            NSNotificationCenter.defaultCenter().addObserver(self, selector: "watchNotification:", name: "watchNotification", object: nil)
-//
-//            }
-//        })
+        //        sc.connect({(success: Bool!, error: NSError!) -> Void in
+        //        if success == true
+        //        {
+        //            self.testURL = NSURL(string: self.sc.url)
+        
+        
+        self.testURL = NSURL(string: "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov")
+        self.sizeView()
+        self.startStream()
+        buttonsDisplayed = true
+        self.button1.tag = 1
+        cv = ControlView(frame: self.view.bounds)
+        self.view.addSubview(cv)
+        //listens to the device orientation, runs orientationChanged when the device is flipped
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "orientationChanged", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        
+        //listens to watchNotifications, runs watchNotification when the device is flipped
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "watchNotification:", name: "watchNotification", object: nil)
+        //
+        //            }
+        //        })
     }
     
     
@@ -56,8 +63,6 @@ class RGBViewController : UIViewController
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.translucent = true
-        
-        
     }
     
     //make the navigation controller bar reappear
@@ -75,17 +80,6 @@ class RGBViewController : UIViewController
     //creates the sizes for each of the aspects of the view so the device can be flipped
     private func sizeView()
     {
-        var orientation = UIDevice.currentDevice().orientation
-        
-        if orientation == UIDeviceOrientation.LandscapeLeft || orientation == UIDeviceOrientation.LandscapeRight
-        {
-            circlebuttonsize = (self.view.bounds.height / 4) * (2/3) as CGFloat
-        }
-        else
-        {
-            circlebuttonsize = (self.view.bounds.width / 4) * (2/3) as CGFloat
-        }
-        
         streamviewframe = CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height)
     }
     
@@ -103,10 +97,6 @@ class RGBViewController : UIViewController
         button.frame = frame
         button.userInteractionEnabled = true
         button.addTarget(self, action:  "buttonPress:", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        //color for debug
-        //button.backgroundColor = UIColor.purpleColor()
-        
         self.view.addSubview(button)
         //println("button created \(button)")
     }
@@ -114,109 +104,14 @@ class RGBViewController : UIViewController
     //the button pressed method, called when one of the buttons is pressed, preforms a segue to a detailed view and passes it the view that was pressed
     func buttonPress(sender : UIButton)
     {
+        println("pressed")
+        showHideControlView()
         //the button and view tags both live in the same namespace so 5,6,7,8 for tags on the views
-        var view : StreamView = self.view.viewWithTag(sender.tag + 4) as! StreamView
-        //println("buttonPressed \(sender.tag)")
-        //println(view)
-        performSegueWithIdentifier("showDetailedView", sender: view)
+        //        var view : StreamView = self.view.viewWithTag(sender.tag + 4) as! StreamView
+        //        performSegueWithIdentifier("showDetailedView", sender: view)
     }
-    
-    //draws a colored circular button at pos xcoor, ycoor
-    private func makeColoredButton(button : UIButton, action : Selector, color : UIColor, xcoor : CGFloat, ycoor : CGFloat)
-    {
-        
-        button.frame = CGRectMake(xcoor, ycoor, circlebuttonsize, circlebuttonsize)
-        button.layer.cornerRadius = 0.5 * button.bounds.size.width
-        button.backgroundColor = color
-        button.addTarget(self, action: action, forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(button)
-        
-        //only blurs in the square frame can't seem to get it to blur a circle
-//        let blur = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
-//        blur.frame = button.frame
-//        blur.layer.cornerRadius = 0.5 * button.bounds.size.width
-//        self.view.addSubview(blur)
-        
-    }
-    
-    
-    //prepares the view for a segue, handles passing the pressed view to the new view controller
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (segue.identifier == "showDetailedView")
-        {
-            var vc : DetailedViewController = segue.destinationViewController as! DetailedViewController
-            vc.detailedView = sender as! StreamView
-            println(vc.detailedView)
-        }
-    }
-    
-    //prepares for the unwind from the detailed view to the normal view. Redraws the view in the correct frame depending on the tag of the view that was passed back
-    @IBAction func prepareForUnwind(segue : UIStoryboardSegue)
-    {
-        if segue.identifier == "unwindToNormalView"
-        {
-            var vc = segue.sourceViewController as! DetailedViewController
-            var view : StreamView = vc.detailedView as StreamView
-            view.frame = streamviewframe
-            self.view.addSubview(view)
-            drawButtons()
-        }
-    }
-    
-    //draws the circular buttons at the bottom of the screen
-    private func drawButtons()
-    {
-        let middlebuttonx : CGFloat = self.view.bounds.width / 2 - circlebuttonsize / 2
-        let circlebuttony : CGFloat = (self.view.bounds.height / 4) * 3 + circlebuttonsize / 4
-        
-        makeColoredButton(redbutton, action: "redButtonPressed", color: UIColor.redColor(), xcoor:  middlebuttonx - 2 * circlebuttonsize, ycoor: circlebuttony)
-        makeColoredButton(bluebutton, action: "blueButtonPressed", color: UIColor.blueColor(), xcoor: middlebuttonx, ycoor: circlebuttony)
-        makeColoredButton(greenbutton, action: "greenButtonPressed", color: UIColor.greenColor(), xcoor: middlebuttonx + 2 * circlebuttonsize, ycoor: circlebuttony)
 
-    }
-    
-    //function called when the red button is pressed
-    func redButtonPressed()
-    {
-        //add code for red button here
-        println("red pressed")
-        sc.socket.emit("change color", 1.0, 0.0, 0.0)
-    }
-    
-    //function called when the blue button is pressed
-    func blueButtonPressed()
-    {
-        //add code for blue button here
-        println("blue pressed")
-        sc.socket.emit("change color", 0.0, 0.0, 1.0)
-    }
-    
-    //function called when the green button is pressed
-    func greenButtonPressed()
-    {
-        //add code for green button here
-        println("green pressed")
-        sc.socket.emit("change color", 0.0, 1.0, 0.0)
-    }
-    
-    //function for handling the notification from the watch. unpacks the sent message and then runs a switch to determine which method to run
-    func watchNotification(notification : NSNotification)
-    {
-        //println("hi from instide the notification")
-        var color = notification.object as! String
-        switch(color)
-        {
-            case "red":
-                redButtonPressed()
-            case "blue":
-                blueButtonPressed()
-            case "green":
-                greenButtonPressed()
-            default:
-                return
-            //shouldn't happen but who knows
-        }
-    }
+
     
     //function called when the device changes orientation
     func orientationChanged()
@@ -238,7 +133,46 @@ class RGBViewController : UIViewController
     {
         sizeView()
         streamview.frame = streamviewframe
-        drawButtons()
     }
+    
+    private func showHideControlView()
+    {
+        if buttonsDisplayed
+        {
+            cv.removeFromSuperview()
+            buttonsDisplayed = false
+        }
+        else
+        {
+            self.view.addSubview(cv)
+            buttonsDisplayed = true
+        }
+    }
+    
+    
+    
+    //prepares the view for a segue, handles passing the pressed view to the new view controller
+    //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //        if (segue.identifier == "showDetailedView")
+    //        {
+    //            var vc : DetailedViewController = segue.destinationViewController as! DetailedViewController
+    //            vc.detailedView = sender as! StreamView
+    //            println(vc.detailedView)
+    //        }
+    //    }
+    
+    //prepares for the unwind from the detailed view to the normal view. Redraws the view in the correct frame depending on the tag of the view that was passed back
+    //    @IBAction func prepareForUnwind(segue : UIStoryboardSegue)
+    //    {
+    //        if segue.identifier == "unwindToNormalView"
+    //        {
+    //            var vc = segue.sourceViewController as! DetailedViewController
+    //            var view : StreamView = vc.detailedView as StreamView
+    //            view.frame = streamviewframe
+    //            self.view.addSubview(view)
+    //            drawButtons()
+    //        }
+    //    }
 
+    
 }
